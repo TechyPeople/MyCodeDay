e33dbffc635a9c2007b2c7e6f09aa8b40bfce1d2
Sanil Chawla <hack@sanil.co>
2015-08-15 16:02:40 -0700

    another thing
    ok

--- selectevent.php
+++ selectevent.php
@@ -1,52 +1,59 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    $userID = $_REQUEST['id'];
+    if ($userID != null) {
+        header('Location: index.php?id=' . $userID);
+    }
+?>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Select an Event // MyCodeDay</title>
-    <meta name="description" content="" />
-    <meta name="author" content="Sanil Chawla" />
-    <link rel="shortcut icon" href="assets/img/favicon.png">
-    <link rel="stylesheet" type="text/css" href="selectevent.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-</head>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<body>
-    <div class="wrapper">
-        <div class="container">
-            <h1>Welcome to MyCodeDay</h1>
-            <form class="form" action="index.php" method="get">
-                <!--                <input type="text" placeholder="Type your event ID..." name="myEvent">-->
-                <input type="text" placeholder="Type your registration ID..." name="userID">
-                <button type="submit" id="login-button">Enter</button>
-                <p>
-                    <br/>You can find your registration ID at the bottom of the confirmation email we sent you when you registered.</p>
-            </form>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Select an Event // MyCodeDay</title>
+        <meta name="description" content="" />
+        <meta name="author" content="Sanil Chawla" />
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+        <link rel="stylesheet" type="text/css" href="selectevent.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div class="wrapper">
+            <div class="container">
+                <h1>Welcome to MyCodeDay</h1>
+                <form class="form" action="index.php" method="get">
+                    <!--                <input type="text" placeholder="Type your event ID..." name="myEvent">-->
+                    <input type="text" placeholder="Type your registration ID..." name="userID">
+                    <button type="submit" id="login-button">Enter</button>
+                    <p>
+                        <br/>You can find your registration ID at the bottom of the confirmation email we sent you when you registered.</p>
+                </form>
+            </div>
+            <script type="text/javascript">
+                $("#login-button").click(function(event) {
+                    //                event.preventDefault();
+
+                    $('form').fadeOut(500);
+                    $('.wrapper').addClass('form-success');
+                });
+
+            </script>
+            <ul class="bg-bubbles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </div>
-        <script type="text/javascript">
-            $("#login-button").click(function(event) {
-                //                event.preventDefault();
+    </body>
 
-                $('form').fadeOut(500);
-                $('.wrapper').addClass('form-success');
-            });
-
-        </script>
-        <ul class="bg-bubbles">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </div>
-</body>
-
-</html>
+    </html>
